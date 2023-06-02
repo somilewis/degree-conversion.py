3c2117ea14587a621c1fa839f2ca1f3a90c4a139
somilewis <87447136+somilewis@users.noreply.github.com>
2023-06-02 07:58:54 -0700

    Add files via upload

---
+++ LAB 5.py
@@ -0,0 +1,26 @@
+# Lewis Muthomi
+# 1250 01
+# 09/19/2022
+# lab 5
+
+
+# This program will convert degrees celsius to degrees fahrenheit
+
+# User greeting
+print('welcome to degrees celsius and degrees fahrenheit program')
+print('please enter temperature in degrees celcius;you may use decimals')
+print()
+
+# Get user input
+celsius = float(input('Enter a temperature in degrees celsius: '))
+                
+# Convert degrees celsius to fahrenheit
+fahrenheit = (celsius * 9 / 5) + 32
+
+
+# Display result
+print(celsius,'degrees celsius is equal to',fahrenheit,'degrees fahrenheit')              
+print()
+
+#exit message
+print('Have a great day!')
